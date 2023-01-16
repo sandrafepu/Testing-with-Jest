f9e0d4d6687917b09bed0bc03cc40e367f95bf45
sandrafepu <sandrafepu@gmail.com>
2023-01-16 11:04:11 +0000

    Cambios en tests

--- test.js
+++ test.js
@@ -10,5 +10,14 @@ test("One euro should be 1.206 dollars", function(){
    expect(fromEuroToDollar(3.5)).toBe(4.2); 
 })
 
+const {fromDollarToYen} = require('./app.js')
+test('One dollar should be 138.88 JPY', function (){
+   expect(fromDollarToYen(1)).toBe(138.88);
+})
+
+const {fromYenToPound} = require('./app.js')
+test('1 yen should be 0.0060 GBP', function (){
+   expect(fromYenToPound(1)).toBe(0.0060);
+})
 
 
